cad2621862fd52a1c3cedac263fa7c523d441879
odan <odan3240@gmail.com>
2021-02-04 23:22:07 +0900

    add sample api

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,9 +1,27 @@
 import express from 'express';
+import { Todo, User } from '../types';
 
 const app = express();
 
 app.get('/hello', (_, res) => {
   res.send('hello');
+});
+
+app.get('/profile', (_, res) => {
+  const profile: User = {
+    id: 1,
+    name: 'Alice',
+  };
+
+  res.send(profile);
+});
+
+app.get('/todo', (_, res) => {
+  const todos: Todo[] = [
+    { title: 'todo1', description: 'todo1 です' },
+    { title: 'todo2', description: 'todo2 です' },
+  ];
+  res.send(todos);
 });
 
 export default app;
---
+++ app/types/index.ts
@@ -0,0 +1,9 @@
+export type User = {
+  id: number;
+  name: string;
+};
+
+export type Todo = {
+  title: string;
+  description: string;
+};
